d4147449c8779ce85d484000a0167251852ec460
K Praveen Kumar <kpkcool@gmail.com>
2022-02-04 17:27:21 +0530

    Initial Commit

---
+++ BaseViewController with Photo Picker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseViewController with Photo Picker
+//
+//  Created by K Praveen Kumar on 04/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
